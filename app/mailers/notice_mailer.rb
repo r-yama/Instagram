1117b0fe6e7fbad86083a13bfb8bbd24a5a307cd
Ryuta Yamashita <r.yamashita69@gmail.com>
2017-05-08 01:43:32 +0000

    Create ActionMailer

---
+++ app/mailers/notice_mailer.rb
@@ -0,0 +1,14 @@
+class NoticeMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notice_mailer.sendmail_post.subject
+  #
+  def sendmail_post(post)
+    @post = post
+
+    mail to: "r.yamashita69@gmail.com"
+         subject: '【Instagram】ブログが投稿されました'
+  end
+end
